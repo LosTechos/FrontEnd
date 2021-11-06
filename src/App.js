58b9da7399cefdd0e2e6c9751a682933996beb1b
CiferU <57766994+CiferU@users.noreply.github.com>
2021-11-05 22:52:38 -0700

    Api-register-house
    House register works with the api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import Login from './Login';
 import Register from './RegUserHouse';
+import axios from "axios";
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      <Login/>
-    </div>
-  );
+const url="https://los-techos.herokuapp.com/api/users"
+
+class App extends Component{
+
+state={
+  data:[]
 }
 
+peticionGet=()=>{
+  axios.get(url).then(response=>{
+    console.log(response.data)
+  })
+}
+componentDidMount(){
+this.peticionGet();
+}
+
+render(){
+  return (
+    <div>
+      <Register/>
+    </div>
+  );
+
+}
+}
 export default App;
+
