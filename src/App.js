414720a414dadcf67c2efad30b39030e70321370
Dave Calderon <56399160+DeividZMBGuy@users.noreply.github.com>
2021-12-02 19:36:39 -0700

    routes-added
    regrese las rutas que estaban pero hay que modificarlas al final

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Pages/Login';
 import Register from './Pages/RegisterForm';
 import Home from './Pages/Dashboard';
-import PaymentsAdmin from './Pages/Ticketadmin';
+import TicketsAdmin from './Pages/Ticketadmin';
 import Profile from "./Pages/Profile";
 import DebUser from './Pages/DebUser.jsx';
-import PaymentsUser from './Pages/TicketsUser'
+import TicketsUser from './Pages/TicketsUser'
 
 
 function App() {
@@ -18,7 +18,8 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="home" element={<Home />} />
         <Route path="register" element={<Register />} />
-        <Route path="payments" element={<PaymentsUser />} />
+        <Route path="ticketsuser" element={<TicketsUser />} />
+        <Route path="ticketsadmin" element={<TicketsAdmin />} />
         <Route path="profile" element={<Profile />} />
         <Route path="debs" element={<DebUser />} />
         
